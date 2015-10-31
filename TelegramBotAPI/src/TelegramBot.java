d11e33a87eb8e25c124a3b26b5ef6bf5ac530e38
OliverSanz <urang.sowelu@gmail.com>
2015-10-31 16:47:34 +0100

    Added "failed" for easy exception control.
    I have serious doubts about this method, but for now the interface will
    use it. It requires that all methods set failed to false, and checks if
    any of its used methods makes failed true. If its happens, it must stop
    executing (for preventing other methods making it false again).

--- TelegramBotAPI/src/TelegramBot.java
+++ TelegramBotAPI/src/TelegramBot.java
@@ -17,6 +17,7 @@ public class TelegramBot {
 	private Iterator<TelegramMsg> msgQueue;
 	private int msgOffSet;
 	private final String botUrl;
+	private boolean failed;
 	
 	/**
 	 * Initializes the bot with its id.
@@ -29,6 +30,8 @@ public class TelegramBot {
 	  	// Initialises the ArrayList and Iterator for the fetched messages.
 		msgs = new ArrayList<TelegramMsg>();
 		msgQueue = msgs.iterator();
+		// failed is true by default
+		failed = true;
 	}
 	
 	/**
@@ -39,16 +42,29 @@ public class TelegramBot {
 	 * @throws	UnsupportedEncodingException
 	 */
 	public void sendText(String text, int chatId) throws UnsupportedEncodingException{
-		String urlString = botUrl +"sendmessage?chat_id="+
-			 chatId+"&text="+URLEncoder.encode(text, "UTF-8");
-		
 		try {
-			new URL(urlString).openStream();
+			failed = false;
+			
+			String urlString = botUrl +"sendmessage?chat_id="+
+			 chatId+"&text=" + URLEncoder.encode(text, "UTF-8");
+			
+			InputStream is = new URL(urlString).openStream();
+			JsonReader rdr = Json.createReader(is);
+			if(!rdr.readObject().getBoolean("ok")){
+				throw new TelegramBotAPIException();
+			}
+			
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
+			failed = true;
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		} catch (UnsupportedEncodingException e){
+			failed = true;
+			e.printStackTrace();
+		} catch (IOException e){
+			failed = true;
+			e.printStackTrace();
+		} catch (TelegramBotAPIException e){
+			failed = true;
 			e.printStackTrace();
 		}
 	}
@@ -56,24 +72,35 @@ public class TelegramBot {
 	/**
 	 * It gets the first unseen message sent to the bot as a TelegramMsg, if there is none,
 	 * it waits some time for one to arrive.
+	 * 
 	 * @param waitTime is the seconds to wait for a new message if there is none
 	 * @return the first unreturned message
 	 */
-	public TelegramMsg nextMsg(int waitTime) throws Exception{
-		if(!msgQueue.hasNext()){
-			msgs.clear();
-			getMessages(waitTime);
+	public TelegramMsg nextMsg(int waitTime){
+		failed = false;
+		
+		try{
+			if(!msgQueue.hasNext()){
+				msgs.clear();
+				getMessages(waitTime);
+				if(failed) throw new Exception();
+			}
+			return msgQueue.next();
+			
+		} catch(Exception e){
+			failed = true;
+			return null;
 		}
-		return msgQueue.next();
 	}
 
 	/**
 	 * Fills msgQueue;
 	 */
 	public void getMessages(int waitTime){
-		URL url;
+		failed = false;
+		
 		try {
-			url = new URL(botUrl + "getupdates?timeout="+waitTime+"&offset=" + msgOffSet);
+			URL url = new URL(botUrl + "getupdates?timeout=" + waitTime + "&offset=" + msgOffSet);
 			InputStream is = url.openStream();
 			JsonReader rdr = Json.createReader(is);
 			JsonObject obj = rdr.readObject();
@@ -83,13 +110,32 @@ public class TelegramBot {
 				msgOffSet = result.getInt("update_id") + 1;
 			}
 			msgQueue = msgs.iterator();
+			
 		}catch(Exception e){
 			e.printStackTrace();
+			failed = true;
 		}
 	}
 	
 	public void throwMessages(){
-		getMessages(0);
-		msgs.clear();
+		failed = false;
+		
+		try{
+			getMessages(0);
+			if(failed) throw new Exception();
+			msgs.clear();
+			
+		}catch(Exception e){
+			failed = true;
+		}
+	}
+	
+	 /**
+	  * Return true if the last called method failed to be completely executed.
+	  * 
+	  * @return ~
+	  */
+	public boolean Failed(){
+		return failed;
 	}
 }
---
+++ TelegramBotAPI/src/TelegramBotAPIException.java
@@ -0,0 +1,9 @@
+
+public class TelegramBotAPIException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
