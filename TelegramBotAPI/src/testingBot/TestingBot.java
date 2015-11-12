b7432acf892c26ad0bc8a1db8377548dd0a7f02b
OliverSanz <urang.sowelu@gmail.com>
2015-11-12 13:37:46 +0100

    Work done and tested

--- TelegramBotAPI/src/testingBot/TestingBot.java
+++ TelegramBotAPI/src/testingBot/TestingBot.java
@@ -5,12 +5,12 @@ import telegramBotApi.*;
 public class TestingBot {
 	public static void main(String[] args){
 		TelegramBot bot = new TelegramBot("137720988:AAHvm9h_RSjiM7klwLZLeuBh-w2scQqJoCI");
-		do bot.throwMessages(); while(bot.failed());
+		while(!bot.throwMessages()){}
 		TelegramMsg msg;
 		
 		while(true){
-			do msg = bot.nextMsg(10); while (bot.failed());
-			do bot.sendText("Holahola", msg.getChat().getId()); while(bot.failed());
+			while((msg = bot.nextMsg(10)) == null){}
+			while(!bot.sendText("Holahola", msg.getChat().getId())){}
 		}
 	}
 }
